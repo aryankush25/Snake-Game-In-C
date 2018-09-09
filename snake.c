fc336a3c334fea0adf3dbdbe60a09edc338e7e81
Aryan Agarwal <aryankush025@gmail.com>
2018-09-10 02:13:30 +0530

    Second Commit

--- snake.c
+++ snake.c
@@ -14,29 +14,34 @@
 #define max_row 15
 #define max_col 30
 
+void delay(float number_of_seconds) {
+    int milli_seconds = 1000 * number_of_seconds;
+    clock_t start_time = clock();
+    while (clock() < start_time + milli_seconds);
+}
+
 struct food {
     int r;
     int c;
     char value;
 };
-
 struct poision {
     int r;
     int c;
     char value;
 };
-
 struct snake {
     int r;
     int c;
     char value;
     struct snake *next;
 };
+
 void addFood(struct food **head) {
     struct food *temp=(struct food *)malloc(sizeof(struct food *));
-    
-    temp->r=(rand() % (max_row - 0 + 1)) + 0;
-    temp->c=(rand() % (max_col - 0 + 1)) + 0;
+
+    temp->r=(rand() % (max_row - 2 )) + 1;
+    temp->c=(rand() % (max_col - 2 )) + 1;
     temp->value = 'F';
     
     *head = temp;
@@ -44,9 +49,9 @@ void addFood(struct food **head) {
 
 void addPoision(struct poision **head) {
     struct poision *temp=(struct poision *)malloc(sizeof(struct poision *));
-    srand(time(0));
-    temp->r=(rand() % (max_row - 0 + 1)) + 0;
-    temp->c=(rand() % (max_col - 0 + 1)) + 0;
+
+    temp->r=(rand() % (max_row - 2 )) + 1;
+    temp->c=(rand() % (max_col - 2 )) + 1;
     temp->value = 'P';
     
     *head = temp;
@@ -74,13 +79,113 @@ void addSnake(struct snake **head,int rr,int cc,char value) {
 }
 
 
-char checkNode(struct snake *head,int i,int j) {
+char checkSnake(struct snake *head, int i, int j) {
     while(head!=NULL) {
         if(head->r==i && head->c==j)
             return head->value;
-        head=head->next;
+        head = head->next;
     }
     return 0;
+}
+
+char checkFood(struct food *head,int i,int j) {
+    if(head->r == i && head->c == j)
+        return head->value;
+    return 0;
+}
+
+char checkPoision(struct poision *head, int i, int j) {
+    if(head->r==i && head->c==j)
+        return head->value;
+    return 0;
+}
+
+void move(struct snake **head, char c) {
+    
+    snake* temp = *head;
+    
+    int r1, c1, r2, c2;
+    
+    r1 = temp->r;
+    c1 = temp->c;
+    
+    switch (c) {
+            
+    case 't' :
+        printf("Top\n");
+        temp->r = r1 - 1;
+        break;
+            
+    case 'b' :
+        printf("Bottom\n");
+        temp->r = r1 + 1;
+        break;
+            
+    case 'r' :
+        printf("Right\n");
+        temp->c = c1+ 1;
+        break;
+            
+    case 'l' :
+        printf("Left\n");
+        temp->c = c1 - 1;
+        break;
+            
+    }
+    
+    temp = temp->next;
+    while(temp != NULL)
+    {
+        r2 = temp->r;
+        c2 = temp->c;
+        
+        temp->r = r1;
+        temp->c = c1;
+        
+        r1 = r2;
+        c1 = c2;
+        
+        temp = temp->next;
+    }
+    
+}
+
+void updateAndShow(char mat[max_row][max_col],struct snake *head,struct food *f,struct poision *p) {
+    for(int i=0;i<max_row;i++)
+    {
+        for(int j=0;j<max_col;j++)
+        {
+            char x = checkSnake(head,i,j);
+            char fo = checkFood(f,i,j);
+            char po = checkPoision(p,i,j);
+            if(i==0||i==(max_row-1)||j==0||j==(max_col-1))
+            {
+                mat[i][j]='#';
+            }
+            else if(x == 'O' || x == '0')
+            {
+                mat[i][j]=x;
+            }
+            else if( fo == 'F' )
+            {
+                mat[i][j]=fo;
+            }
+            else if( po == 'P' )
+            {
+                mat[i][j]=po;
+            }
+            else
+                mat[i][j]=' ';
+        }
+    }
+    for(int i=0;i<max_row;i++)
+    {
+        for(int j=0;j<max_col;j++)
+        {
+            printf("%c",mat[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main(int argc, const char * argv[]) {
@@ -88,44 +193,72 @@ int main(int argc, const char * argv[]) {
     struct snake *head=NULL;
     struct food *f = NULL;
     struct poision *p = NULL;
-    
+    srand(time(0));
     addFood(&f);
     addPoision(&p);
     
-    addSnake(&head,10,7,'o');
-    for(int i=1; i<5;i++)
-    {
-        addSnake(&head,10-i,7,'*');
+    addSnake(&head,10,7,'0');
+    for(int i=1; i<4;i++) {
+        addSnake(&head,10-i,7,'O');
     }
     
-    int i,j;
     char mat[max_row][max_col];
     
-    for(i=0;i<max_row;i++)
-    {
-        for(j=0;j<max_col;j++)
-        {
-            char x = checkNode(head,i,j);
-            if(i==0||i==(max_row-1)||j==0||j==(max_col-1))
-            {
-                mat[i][j]='#';
-            }
-            else if(x == '*' || x == 'o')
-            {
-                mat[i][j]=x;
-            }
-            else
-                mat[i][j]=' ';
+    updateAndShow(mat,head,f,p);
+    
+    int direction = 0; // 0 for vertical & 1 for horizontal
+    
+    char input;
+    scanf("%c",&input);
+    
+    while (input != 'q') {
+        
+        switch (input) {
+                
+            case 'w':
+            case 'W':
+                if(direction==1) {
+                    move(&head, 't');
+                    delay(3);
+                    direction=0;
+                    updateAndShow(mat,head,f,p);
+                }
+                break;
+                
+            case 'd':
+            case 'D':
+                if(direction==0) {
+                    move(&head, 'r');
+                    delay(3);
+                    direction=1;
+                    updateAndShow(mat,head,f,p);
+                }
+                break;
+                
+            case 's':
+            case 'S':
+                if(direction==1) {
+                    move(&head, 'b');
+                    delay(3);
+                    direction=0;
+                    updateAndShow(mat,head,f,p);
+                }
+                break;
+                
+            case 'a':
+            case 'A':
+                if(direction==0) {
+                    move(&head, 'l');
+                    delay(3);
+                    direction=1;
+                    updateAndShow(mat,head,f,p);
+                }
+                break;
         }
+        scanf("%c", &input);
     }
-    for(i=0;i<max_row;i++)
-    {
-        for(j=0;j<max_col;j++)
-        {
-            printf("%c",mat[i][j]);
-        }
-        printf("\n");
-    }
+    
+    
     
     return 0;
 }
